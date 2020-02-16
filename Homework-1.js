4b82f89d686e173c728c315f7b009691277debe6
codecamp-official-thailand <codecamp.official.thailand@gmail.com>
2020-02-16 23:36:56 +0700

    Homework 1

---
+++ Homework-1.js
@@ -0,0 +1,48 @@
+// 1. จงคำนวณค่าด้านล่างต่อไปนี้ และ คอมเม้นตอบหลังคำถาม
+5 + "34"            // ตอบตรงนี้
+5 ** 4              // ตอบตรงนี้
+5 - "4"             // ตอบตรงนี้
+10 % 5
+5 % 10;
+(2 ** 5) % 2
+"Java" + "Script"
+" " + " "
+" " + 0
+20 + 4 + "15"
+true + true
+true + false
+false + true
+false - true
+false - true + false - true + false;
+(true + true) ** 4
+19 + true + "20"
+3 - 4
+"Bob" - "bill"
+"Code" + "Camp" + true + false + null + undefined
+
+
+// 2. จงคำนวณค่าเปรียบเทียบต่อไปนี้ และ คอมเม้นตอบด้านหลังเหมือนข้างบน
+5 >= 1
+0 === 1
+4 <= 1
+1 != 1
+1 == "1"
+1 === "1"
+"2" == "2"
+"2" === "2"
+"12" !== 12
+"15" != 15
+"A" > "B"
+"B" < "C"
+"a" > "A"
+"b" < "A"
+"bee" > "bbe"
+"BeE" < "Bee"
+"SonTer" > "Soncom"
+"circle" > "circles"
+"NaRuTo" < "nArUtO"
+true === false
+true != true
+
+
+// 3. สร้าง String ที่มีค่า "Hello, It's me. "Mario"."
