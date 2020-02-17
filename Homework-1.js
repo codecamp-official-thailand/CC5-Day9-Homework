7efaf651f021be738e1cd65112a2950b6797a459
Hon <dennis.mejen@gmail.com>
2020-02-17 16:02:37 +0700

    Add answers

--- Homework-1.js
+++ Homework-1.js
@@ -1,48 +1,49 @@
 // 1. จงคำนวณค่าด้านล่างต่อไปนี้ และ คอมเม้นตอบหลังคำถาม
-5 + "34"            // ตอบตรงนี้
-5 ** 4              // ตอบตรงนี้
-5 - "4"             // ตอบตรงนี้
-10 % 5
-5 % 10;
-(2 ** 5) % 2
-"Java" + "Script"
-" " + " "
-" " + 0
-20 + 4 + "15"
-true + true
-true + false
-false + true
-false - true
-false - true + false - true + false;
-(true + true) ** 4
-19 + true + "20"
-3 - 4
-"Bob" - "bill"
-"Code" + "Camp" + true + false + null + undefined
+5 + "34"            // ตอบตรงนี้ 534
+5 ** 4              // ตอบตรงนี้ 625
+5 - "4"             // ตอบตรงนี้ 1
+10 % 5              // ตอบตรงนี้ 0
+5 % 10;             // ตอบตรงนี้ 5
+(2 ** 5) % 2        // ตอบตรงนี้ 0
+"Java" + "Script"   // ตอบตรงนี้ JavaScript
+" " + " "           // ตอบตรงนี้   
+" " + 0             // ตอบตรงนี้  0
+20 + 4 + "15"       // ตอบตรงนี้ 2415
+true + true         // ตอบตรงนี้ 2
+true + false        // ตอบตรงนี้ 1
+false + true        // ตอบตรงนี้ 1
+false - true        // ตอบตรงนี้ -1
+false - true + false - true + false;    // ตอบตรงนี้ -2
+(true + true) ** 4  // ตอบตรงนี้ 16
+19 + true + "20"    // ตอบตรงนี้ 2020
+3 - 4               // ตอบตรงนี้ -1
+"Bob" - "bill"      // ตอบตรงนี้ NaN
+"Code" + "Camp" + true + false + null + undefined   // ตอบตรงนี้ CodeCamptruefalsenullundefined
 
 
 // 2. จงคำนวณค่าเปรียบเทียบต่อไปนี้ และ คอมเม้นตอบด้านหลังเหมือนข้างบน
-5 >= 1
-0 === 1
-4 <= 1
-1 != 1
-1 == "1"
-1 === "1"
-"2" == "2"
-"2" === "2"
-"12" !== 12
-"15" != 15
-"A" > "B"
-"B" < "C"
-"a" > "A"
-"b" < "A"
-"bee" > "bbe"
-"BeE" < "Bee"
-"SonTer" > "Soncom"
-"circle" > "circles"
-"NaRuTo" < "nArUtO"
-true === false
-true != true
+5 >= 1          // true
+0 === 1         // false
+4 <= 1          // false
+1 != 1          // false
+1 == "1"        // true
+1 === "1"       // false
+"2" == "2"      // true
+"2" === "2"     // true
+"12" !== 12     // true
+"15" != 15      // false
+"A" > "B"       // false
+"B" < "C"       // true
+"a" > "A"       // true
+"b" < "A"       // false
+"bee" > "bbe"   // true
+"BeE" < "Bee"   // true
+"SonTer" > "Soncom"     // false
+"circle" > "circles"    // false
+"NaRuTo" < "nArUtO"     // true
+true === false  // false
+true != true    // false
 
 
 // 3. สร้าง String ที่มีค่า "Hello, It's me. "Mario"."
+`"Hello, It's me. "Mario"."`
