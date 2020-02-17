2234f57c12f29eefa3cd0f92a177bb25369ad4fb
ZEN <zen.kiattikhunwawong.com>
2020-02-17 15:57:27 +0700

    Test

--- Homework-1.js
+++ Homework-1.js
@@ -1,24 +1,24 @@
 // 1. จงคำนวณค่าด้านล่างต่อไปนี้ และ คอมเม้นตอบหลังคำถาม
-5 + "34"            // ตอบตรงนี้
-5 ** 4              // ตอบตรงนี้
-5 - "4"             // ตอบตรงนี้
-10 % 5
-5 % 10;
-(2 ** 5) % 2
-"Java" + "Script"
-" " + " "
-" " + 0
-20 + 4 + "15"
-true + true
-true + false
-false + true
-false - true
-false - true + false - true + false;
-(true + true) ** 4
-19 + true + "20"
-3 - 4
-"Bob" - "bill"
-"Code" + "Camp" + true + false + null + undefined
+5 + "34"            // 534
+5 ** 4              // 625
+5 - "4"             // 1
+10 % 5              // 0
+5 % 10;             // 0
+(2 ** 5) % 2        // 0
+"Java" + "Script"   // JavaScript
+" " + " "             //
+" " + 0             //
+20 + 4 + "15"             //
+true + true             //
+true + false             //
+false + true             //
+false - true             //
+false - true + false - true + false;             //
+(true + true) ** 4             //
+19 + true + "20"             //
+3 - 4             //
+"Bob" - "bill"             //
+"Code" + "Camp" + true + false + null + undefined             //
 
 
 // 2. จงคำนวณค่าเปรียบเทียบต่อไปนี้ และ คอมเม้นตอบด้านหลังเหมือนข้างบน
