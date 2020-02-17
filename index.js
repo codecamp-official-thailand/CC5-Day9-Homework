d54d6d20e22fe7d6401169a718984e1c2c410ee5
ZEN <zen.kiattikhunwawong.com>
2020-02-17 11:31:35 +0700

    backup

--- index.js
+++ index.js
@@ -8,7 +8,33 @@
 // console.log(human);
 
 //Lab 2
-let myMoney;
-let myParentName;
-let myAddress;
-let galaxyAge;
+// let myMoney;
+// let myParentName;
+// let myAddress;
+// let galaxyAge;
+
+// console.log(typeof(-Infinity));
+// console.log(Infinity/Infinity);
+// console.log(NaN/NaN);
+
+
+//Exercise 1
+// let name = "Test";
+// console.log(`hello ${1}`);
+// console.log(`hello ${"name"}`);
+// console.log(`hello ${name}`);
+
+//Exercise 2
+// let myName = "ZEN";
+// let myAge = 22;
+// let myAddress = "0 Bangkok";
+// let myBio =`My name is ${myName}\n I'm ${myAge} year old.\n My address: ${myAddress}`;
+
+// console.log(myBio);
+
+//Exercise 3
+let var1 = undefined;
+
+var1 = String(var1);
+console.log(typeof var1)
+console.log(var1)
