5de502625d2965bfcc5c07f186094b3f9ade06f0
ZEN <zen.kiattikhunwawong.com>
2020-02-17 10:42:25 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Lab 1
+// let human;
+// let name;
+
+// name = "ZEN 17/2/2020";
+// human = name;
+
+// console.log(human);
+
+//Lab 2
+let myMoney;
+let myParentName;
+let myAddress;
+let galaxyAge;
