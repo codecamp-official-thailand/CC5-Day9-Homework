b6625f0c8ab949dfea6aaf1126ddfd2d27f68601
Suchada manowon <suchada.m482@gmail.com>
2020-04-15 22:55:07 +0700

    javascript basic homework
    add file index.js (solution)

---
+++ index.js
@@ -0,0 +1,50 @@
+// // 1. จงคำนวณค่าด้านล่างต่อไปนี้ และ คอมเม้นตอบหลังคำถาม
+//let response = 5 + "34"
+//let response = 5 ** 4 
+//let response = 5 - "4"
+// let response =10 % 5              
+// let response = 5 % 10;            
+// let response = (2 ** 5) % 2        
+// let response = "Java" + "Script"   
+// let response = " " + " "           
+// let response = " " + 0             
+// let response = 20 + 4 + "15"       
+// let response = true + true         
+// let response = true + false        
+// let response = false + true        
+// let response = false - true        
+// let response = false - true + false - true + false;        
+// let response = (true + true) ** 4      
+// let response = 19 + true + "20"        
+// let response = 3 - 4
+// let response = "Bob" - "bill"
+// let response = "Code" + "Camp" + true + false + null + undefined
+
+// // 2. จงคำนวณค่าเปรียบเทียบต่อไปนี้ และ คอมเม้นตอบด้านหลังเหมือนข้างบน
+// let response = 5 >= 1            
+// let response = 0 === 1
+// let response = 4 <= 1
+// let response = 1 != 1
+// let response = 1 == "1"
+// let response = 1 === "1"
+// let response = "2" == "2"
+// let response = "2" === "2"
+// let response = "12" !== 12
+// let response = "A" > "B"
+// let response = "B" < "C"
+// let response = "a" > "A"
+// let response = "b" < "A"
+// let response = "bee" > "bbe"
+// let response = "BeE" < "Bee"
+// let response = "SonTer" > "Soncom"
+// let response = "circle" > "circles"
+// let response = "NaRuTo" < "nArUtO"
+// let response = true === false
+//  let response = true != true
+
+
+
+// console.log(response);
+
+let name = "Mario";
+console.log("Hello, It's me. " + name);
