326e1a58e1e94823fb1b7c333c2c3731d4b9e4b8
Boss0049 <B-Bossza@hotmail.co.th>
2020-05-27 19:32:56 +0700

    HomeWork_JS

--- Homework-1.js
+++ Homework-1.js
@@ -1,46 +1,49 @@
 // 1. จงคำนวณค่าด้านล่างต่อไปนี้ และ คอมเม้นตอบหลังคำถาม
-5 + "34"; // ตอบตรงนี้
-5 ** 4; // ตอบตรงนี้
-5 - "4"; // ตอบตรงนี้
-10 % 5; // ตอบแล้ว
-5 % 10;
-2 ** 5 % 2;
-"Java" + "Script";
-" " + " ";
-" " + 0;
-20 + 4 + "15";
-true + true;
-true + false;
-false + true;
-false - true;
-false - true + false - true + false;
-(true + true) ** 4;
-19 + true + "20";
-3 - 4;
-"Bob" - "bill";
-"Code" + "Camp" + true + false + null + undefined;
+5 + "34"; // 534
+5 ** 4; // 625
+5 - "4"; // 1
+10 % 5; // 0
+5 % 10; // 5
+2 ** 5 % 2; // 0
+"Java" + "Script"; // JavaScript
+" " + " "; // " "
+" " + 0; // 0
+20 + 4 + "15"; //2415
+true + true; // 2
+true + false; // 1
+false + true; // 1
+false - true; // -1
+false - true + false - true + false; // -2
+(true + true) ** 4; //16
+19 + true + "20"; // 2020
+3 - 4; // -1
+"Bob" - "bill"; // NaN
+"Code" + "Camp" + true + false + null + undefined; // CodeCamptruefalsenullundefined
 
 // 2. จงคำนวณค่าเปรียบเทียบต่อไปนี้ และ คอมเม้นตอบด้านหลังเหมือนข้างบน
-5 >= 1;
-0 === 1;
-4 <= 1;
-1 != 1;
-1 == "1";
-1 === "1";
-"2" == "2";
-"2" === "2";
-"12" !== 12;
-"15" != 15;
-"A" > "B";
-"B" < "C";
-"a" > "A";
-"b" < "A";
-"bee" > "bbe";
-"BeE" < "Bee";
-"SonTer" > "Soncom";
-"circle" > "circles";
-"NaRuTo" < "nArUtO";
-true === false;
-true != true;
+5 >= 1; // T
+0 === 1; // F
+4 <= 1; // F
+1 != 1; // F
+1 == "1"; // T
+1 === "1"; // F
+"2" == "2"; // T
+"2" === "2"; // T
+"12" !== 12; // T
+"15" != 15; // F
+"A" > "B"; // F
+"B" < "C"; // T
+"a" > "A"; // T
+"b" < "A"; // F
+"bee" > "bbe"; // T
+"BeE" < "Bee"; // T
+"SonTer" > "Soncom"; // F
+"circle" > "circles"; // F
+"NaRuTo" < "nArUtO"; // T
+true === false; // F
+true != true; // F
 
 // 3. สร้าง String ที่มีค่า "Hello, It's me. "Mario"."
+let hello = "Hello";
+let world = `${hello}, It's me. "Mario". `;
+console.log(world);
