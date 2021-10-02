4cfbbaaec068ec81e73ee6947de0eef3a0adea47
sahaphap2538 <90331935+sahaphap2538@users.noreply.github.com>
2021-10-02 23:07:00 +0700

    Update Homework-1.js
    ส่งการบ้าน #codecamp10 นายสหภาพ เนาวะราช

--- Homework-1.js
+++ Homework-1.js
@@ -1,48 +1,98 @@
+
 // 1. จงคำนวณค่าด้านล่างต่อไปนี้ และ คอมเม้นตอบหลังคำถาม
-5 + "34"            // ตอบตรงนี้
-5 ** 4              // ตอบตรงนี้
-5 - "4"             // ตอบตรงนี้
-10 % 5
-5 % 10;
-(2 ** 5) % 2
-"Java" + "Script"
-" " + " "
-" " + 0
-20 + 4 + "15"
-true + true
-true + false
-false + true
-false - true
-false - true + false - true + false;
-(true + true) ** 4
-19 + true + "20"
-3 - 4
-"Bob" - "bill"
-"Code" + "Camp" + true + false + null + undefined
-
-
+2
+5 + "34"            // 534
+3
+5 ** 4              // 625
+4
+5 - "4"             // 1
+5
+10 % 5              // 0
+6
+5 % 10;             // 5
+7
+(2 ** 5) % 2        // 0
+8
+"Java" + "Script"   //JavaScript
+9
+" " + " "           // "  "
+10
+" " + 0             //  0
+11
+20 + 4 + "15"       // 2415
+12
+true + true         // 2
+13
+true + false        // 1
+14
+false + true        // 1
+15
+false - true        // -1
+16
+false - true + false - true + false;   // -2
+17
+(true + true) ** 4    // 16
+18
+19 + true + "20"      // 2020
+19
+3 - 4                 // -1
+20
+"Bob" - "bill"        // NaN
+21
+"Code" + "Camp" + true + false + null + undefined  //CodeCamptruefalsenullundefined
+22
+​
+23
+​
+24
 // 2. จงคำนวณค่าเปรียบเทียบต่อไปนี้ และ คอมเม้นตอบด้านหลังเหมือนข้างบน
-5 >= 1
-0 === 1
-4 <= 1
-1 != 1
-1 == "1"
-1 === "1"
-"2" == "2"
-"2" === "2"
-"12" !== 12
-"15" != 15
-"A" > "B"
-"B" < "C"
-"a" > "A"
-"b" < "A"
-"bee" > "bbe"
-"BeE" < "Bee"
-"SonTer" > "Soncom"
-"circle" > "circles"
-"NaRuTo" < "nArUtO"
-true === false
-true != true
-
-
+25
+5 >= 1  //true
+26
+0 === 1  //false
+27
+4 <= 1   //false
+28
+1 != 1    //false
+29
+1 == "1"  //true
+30
+1 === "1"  //false
+31
+"2" == "2" //true
+32
+"2" === "2" //true
+33
+"12" !== 12 //true
+34
+"15" != 15  //false
+35
+"A" > "B"  //false
+36
+"B" < "C"  //true
+37
+"a" > "A"  //true
+38
+"b" < "A"  //false
+39
+"bee" > "bbe"  //true
+40
+"BeE" < "Bee"  //true
+41
+"SonTer" > "Soncom"  //false
+42
+"circle" > "circles"  //false
+43
+"NaRuTo" < "nArUtO"  //true
+44
+true === false  //false
+45
+true != true    //false
+46
+​
+47
+​
+48
 // 3. สร้าง String ที่มีค่า "Hello, It's me. "Mario"."
+49
+'"Hello,It'+"'"+'s me."Mario"."'
